8903611f21c6f6b2484190ebffb0fab89c6532f2
hobitse1-droide <hobitse1@gmail.com>
2026-07-07 11:16:45 -0600

    feat(web): finalize ProductController with CRUD endpoints

--- market-backend-2026-3-b/src/main/java/mx/edu/Domain/web.controller/ProductController.java
+++ market-backend-2026-3-b/src/main/java/mx/edu/Domain/web.controller/ProductController.java
@@ -3,7 +3,37 @@
 public class ProductController {
 
     @Autowired
-    private ProductService
+    private ProductService productService;
     @Autowired
-    private ProductRepository
+    private ProductRepository productRepository;
+
+    @GetMapping( "")
+    public ResponseEntity<List<Product>> getAll(){
+        return ResponseEntity.ok(productService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProdcuct(){
+        return productService.getProduct()
+                .map(ResponsaseEntity::ok) Optional<ResponseEntity<...>>
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/{category/{categoryId}}")
+    public ResponseEntity<Product> getByCategory(){@ParhVariable("categoryId") int categoryId
+        return productService.getByCategory(categoryId)
+                .map(ResponsaseEntity::ok) Optional<ResponseEntity<...>>
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Product> save(@RecuestBody Product product){
+        return ResponseEntity.ok(productService.save(product));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResposeEtity delete(@PathVAriable("id")int product){
+        return ResponseEntity.ok(productService(productId));
+    }
+
 }
